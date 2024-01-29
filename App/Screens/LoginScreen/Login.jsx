3bdd5bf4dab20ab5d72b269ee622e2b9459d8d46
Kousikgvs <kousikgvs@gmail.com>
2024-01-29 09:23:44 +0530

    Completed the Login Page

--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import Colors from '../../utils/Colors';
 
@@ -7,8 +7,15 @@ const Login = () => {
     <View style={{alignItems:"center"}}>
         <Image source={{ uri: 'https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?w=740&t=st=1706498472~exp=1706499072~hmac=5388be9d4b1084292a1e8b2d9299809066d7d0c23eebc3950fa8aa7b750c642f' }} style={styles.loginImage} />
         <View style={styles.subContainer}>
-            <Text>Hello</Text>
-        </View>
+            <Text style={{fontSize:25 , color:Colors.WHITE  , textAlign:"center"}}
+            >Lets Find {""}
+            <Text style={{fontWeight:"bold"}}>Professional cleaning and Repair Services</Text>
+            </Text>
+            <Text style={{fontSize:16 , color:Colors.WHITE , textAlign:"center" , marginTop:20}}>Best App to find services near you which deliver you a Professional service</Text>
+            <TouchableOpacity style={styles.button} onPress={() => console.log("pressed the Button")}>
+                <Text style={{textAlign:"center" , color:Colors.PRIMARY}}>Let's get Started</Text>
+            </TouchableOpacity>
+        </View> 
     </View>
   ); 
 }
@@ -33,5 +40,12 @@ const styles = StyleSheet.create({
         borderWidth:4,
         borderColor:Colors.BLACK,
         borderRadius:4,
+        padding:20,
+     },
+     button:{
+        padding:15,
+        backgroundColor:Colors.WHITE,
+        borderRadius:99,
+        marginTop:30,
      }
 });
